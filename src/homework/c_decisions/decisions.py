d19c57ab17a586805cbc18cd0b1f71dd0cb6413f
coleestrin <157080454+coleestrin@users.noreply.github.com>
2024-02-09 20:51:11 +0000

    Homework 2

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -1,1 +1,14 @@
-#
+def get_options_ratio(options, total):
+    return options / total
+
+def get_faculty_rating(ratio):
+    if (ratio > 0.9 and ratio < 1): 
+        return "Excellent"
+    elif (ratio > 0.8): 
+        return "Very Good"
+    elif (ratio > 0.7):
+        return "Good"
+    elif (ratio > 0.6): 
+        return "Needs Improvement"
+    elif (ratio > 0): 
+        return "Unacceptable"
--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,1 +1,10 @@
-#
+from decisions import get_faculty_rating
+from decisions import get_options_ratio
+
+
+options = float(input("Options?"))
+total = float(input("Total?"))
+
+result = get_options_ratio(options, total)
+
+print(get_faculty_rating(result))
