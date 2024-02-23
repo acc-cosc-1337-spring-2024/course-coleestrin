ffbd2a746f3708769f4e30b62744ec4f52b7efe6
coleestrin <157080454+coleestrin@users.noreply.github.com>
2024-02-23 19:33:03 +0000

    Homework 4

--- src/homework/e_functions/main.py
+++ src/homework/e_functions/main.py
@@ -1,1 +1,16 @@
-#
+from value_return import get_hour
+from value_return import get_minutes
+from value_return import get_seconds
+
+def get_time(epoch_seconds):
+    hours = str(get_hour(epoch_seconds))
+    minutes = str(get_minutes(epoch_seconds))
+    seconds = str(get_seconds(epoch_seconds))
+
+    hours = "0" + hours if len(hours) == 1 else hours
+    minutes = "0" + minutes if len(minutes) == 1 else minutes
+    seconds = "0" + seconds if len(seconds) == 1 else seconds
+
+    return hours + ":" + minutes + ":" + seconds
+
+print(get_time(3800))
--- src/homework/e_functions/value_return.py
+++ src/homework/e_functions/value_return.py
@@ -1,1 +1,8 @@
-#
+def get_hour(epoch_seconds): 
+    return epoch_seconds // 3600
+
+def get_minutes(epoch_seconds):
+    return (epoch_seconds - get_hour(epoch_seconds) * 3600) // 60
+
+def get_seconds(epoch_seconds):
+    return epoch_seconds - (get_minutes(epoch_seconds) * 60) - (get_hour(epoch_seconds) * 3600)
