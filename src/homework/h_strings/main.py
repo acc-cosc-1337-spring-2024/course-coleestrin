2b61af7d36096d22acc9ac6ec1fd2ee06049ef01
coleestrin <157080454+coleestrin@users.noreply.github.com>
2024-03-01 20:30:38 +0000

    Homework 5

--- src/homework/h_strings/main.py
+++ src/homework/h_strings/main.py
@@ -1,1 +1,28 @@
-#
+from strings import get_dna_complement, get_hamming_distance
+
+def menu():
+
+    print("""
+    1. Hamming Distance
+    2. DNA Complement
+    3. Exit
+    """)
+
+    userChoice = int(input("What do you pick? (1-3)"))
+
+    if (userChoice == 3):
+        return
+
+    if (userChoice == 2):
+        inputComplement = input("What string do you want the complement of?")
+        print("The complement is " + get_dna_complement(inputComplement))
+
+    if (userChoice == 1):
+        inputHamming1 = input("First string?")
+        inputHamming2 = input("Second string?")
+        print("The hamming distance is " + str(get_hamming_distance(inputHamming1, inputHamming2)))
+
+
+    menu()
+
+menu()
--- src/homework/h_strings/strings.py
+++ src/homework/h_strings/strings.py
@@ -1,1 +1,28 @@
-#
+def get_hamming_distance(dna1, dna2):
+    hamming_distance = 0
+    i = 0
+
+    while (i < len(dna1)):
+        if dna1[i] != dna2[i]:
+            hamming_distance += 1
+
+        i += 1
+    
+    return hamming_distance
+
+def get_dna_complement(dna):
+    complement = ""
+
+    for char in dna:
+        if char == "A":
+            complement = "T" + complement
+        elif char == "C":
+            complement = "G" + complement
+        elif char == "T":
+            complement = "A" + complement
+        elif char == "G":
+            complement = "C" + complement
+        else:
+            complement = char + complement
+
+    return complement        
